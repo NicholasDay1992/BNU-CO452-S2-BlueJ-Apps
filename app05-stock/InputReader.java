9d804c2e0518cf4f8695f45da119af00a2c83755
NicholasDay1992 <48986277+nickday1992@users.noreply.github.com>
2021-03-10 13:12:39 +0000

    changed nextLine() to next()

--- app05-stock/InputReader.java
+++ app05-stock/InputReader.java
@@ -2,12 +2,12 @@ import java.util.HashSet;
 import java.util.Scanner;
 
 /**
- * InputReader reads typed text input from the standard text terminal. 
+ * InputReader reads typed text input from the standard text terminal.
  * The text typed by a user is returned.
- * 
+ *
  * @author     Michael Kölling and David J. Barnes
  * @version    0.1 (2016.02.29)
- * 
+ *
  * Modified by Derek Peacock 13/12/2020
  */
 public class InputReader
@@ -32,42 +32,42 @@ public class InputReader
     {
         String inputLine = null;
         boolean isValid = false;
-        
+
         while(!isValid)
         {
             System.out.print(prompt);         // print prompt
-            inputLine = reader.nextLine();
-            
+            inputLine = reader.next();
+
             if(!inputLine.isEmpty())
                 isValid = true;
-            else 
+            else
                 System.out.println("Your entry is blank!");
         }
 
         return inputLine;
     }
-    
+
     /**
      * Read a the next int from standard imput (the text terminal),
      * and return it as an interger.
-     * 
+     *
      * @return Integer typed by user.
      */
     public int getInt(String prompt)
     {
         int number = 0;
         boolean isValid = false;
-        
+
         while(!isValid)
         {
             System.out.println(prompt);         // print prompt
             number = reader.nextInt();
-            
+
             if(number > 0)
             {
                 isValid = true;
             }
-            else 
+            else
                 System.out.println("Your value is less than zero!");
         }
 
